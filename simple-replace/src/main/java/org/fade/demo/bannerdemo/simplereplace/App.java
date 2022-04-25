9b1010a34a5a3bc9ae45b7859d7762715c83e9e6
paigeman <53284808+paigeman@users.noreply.github.com>
2022-04-25 17:42:08 +0800

    simple-replace module

---
+++ simple-replace/src/main/java/org/fade/demo/bannerdemo/simplereplace/App.java
@@ -0,0 +1,19 @@
+package org.fade.demo.bannerdemo.simplereplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用程序
+ *
+ * @author fade
+ * @date 2022/04/25
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
